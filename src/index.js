f0289c935d3059842e8e0721dff61242c0101e07
LAASYA <71040750+laasya2005@users.noreply.github.com>
2024-07-22 15:19:57 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'tachyons';
+
+
+ReactDOM.render(<App />, document.getElementById('root')
+
+  );
+
+// Remove or comment out the following line
+// registerServiceWorker();
+
